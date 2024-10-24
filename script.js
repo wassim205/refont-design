cb6da1b16b660e67a7daf60981065131e6c35de0
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2024-10-24 16:25:29 +0100

    RD-8 blog page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// images popup
 function toggleImage(src) {
   const popup = document.getElementById("image-popup");
   const img = document.getElementById("popup-image");
@@ -20,3 +21,53 @@ function toggleImage(src) {
     img.src = ""; // Clear the image source when closing
   }
 }
+
+// vedio popup
+
+function toggleVideo() {
+  const popup = document.getElementById("video-popup");
+  const videoFrame = document.getElementById("video-frame");
+
+  if (popup.classList.contains("hidden")) {
+    videoFrame.src = "https://www.youtube.com/embed/5ouVWbW7SsM"; // Replace with your video ID
+    popup.classList.remove("hidden");
+  } else {
+    videoFrame.src = ""; // Stop the video
+    popup.classList.add("hidden");
+  }
+}
+
+
+// show or hide p
+
+function toggleReadMore(paragraphId, event) {
+  const hiddenParagraph = document.getElementById(paragraphId);
+  const readMore = event.currentTarget; // This will refer to the clicked div
+
+  hiddenParagraph.classList.toggle('hidden'); // Toggle the hidden class
+
+  // Update the "Read More" text to "Read Less"
+  if (hiddenParagraph.classList.contains('hidden')) {
+      readMore.textContent = 'Read More ...';
+  } else {
+      readMore.textContent = 'Read Less ...';
+  }
+}
+
+
+
+// function toggleReadMore() {
+//   const readMore = document.getElementById('readMore');
+//   const hiddenParagraph = readMore.parentElement.querySelector('#hiddenParagraph');
+
+  
+//   // Toggle the 'hidden' class on the hidden paragraph
+//   hiddenParagraph.classList.toggle('hidden');
+  
+//   // Update the "Read More" text to "Read Less" and vice versa
+//   if (hiddenParagraph.classList.contains('hidden')) {
+//       readMore.textContent = 'Read More ...';
+//   } else {
+//       readMore.textContent = 'Read Less ...';
+//   }
+// }
